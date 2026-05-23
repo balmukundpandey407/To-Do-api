37608cc6b6303ef8d4876871e2e6ed3a753b3268
BALUKUND PANDEY <236516530+balmukundpandey407@users.noreply.github.com>
2026-05-23 15:45:21 +0530

    convert string to datetime

--- app/schemas/task.py
+++ app/schemas/task.py
@@ -1,20 +1,21 @@
 from pydantic import BaseModel, Field
 from typing import Literal,Optional, Annotated
+from datetime import datetime
 
 class Task(BaseModel):
     id: str 
     title: Annotated[str, Field(..., max_length=100, title="Title of the task")]
     description: Annotated[Optional[str], Field(default=None, max_length=200, title="Description of the task")]
-    completion: Annotated[Literal['Done','Pending'], Field(title="Completion status of the task",default='Pending')]
-    created_at: str
-    updated_at: str
+    status: Annotated[Literal['Done','Pending'], Field(title="Completion status of the task",default='Pending')]
+    created_at: datetime
+    updated_at: datetime
 
 class TaskCreate(BaseModel):
     title: str
     description: Optional[str] = None
-    completion: Literal['Done','Pending'] = 'Pending'
+    status: Literal['Done','Pending'] = 'Pending'
 
 class TaskUpdate(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
-    completion: Optional[Literal['Done','Pending']] = None
+    status: Optional[Literal['Done','Pending']] = None
