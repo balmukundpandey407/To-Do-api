80fc71b3dcb52e4324ce957bca14051159124bf3
BALUKUND PANDEY <236516530+balmukundpandey407@users.noreply.github.com>
2026-05-23 15:47:06 +0530

    change table name

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/todo_app"
 
 # Create engine
 engine = create_engine(DATABASE_URL)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column,String
+from sqlalchemy import Column,String,DateTime
 from sqlalchemy.ext.declarative import declarative_base
 
 # Base class for models
@@ -6,11 +6,11 @@ Base = declarative_base()
 
 class Task(Base):
     
-    __tablename__ = "TO DO list"
+    __tablename__ = "tasks"
 
     id = Column(String, primary_key=True)
     title = Column(String, nullable=False)
     description = Column(String, nullable=True)
     status = Column(String, nullable=False)
-    created_at = Column(String, nullable=False)
-    updated_at = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
