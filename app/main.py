f1767b037c0c36d82b6e17370b53bb18718a047c
BALUKUND PANDEY <236516530+balmukundpandey407@users.noreply.github.com>
2026-05-28 21:44:17 +0530

    add athorization

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from routes.tasks import router as task_router
+from routes.tasks import task_router
+from routes.auth import auth_router
 
 app = FastAPI()
 
@@ -8,3 +9,4 @@ def home():
     return {"message": "TO DO API IS WORKING PROPERLY"}
 
 app.include_router(task_router)
+app.include_router(auth_router,tags=["AUTHENTICATION_ROUTES"])
