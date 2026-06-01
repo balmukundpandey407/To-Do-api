952fd2247ef5560b20663c5a0b242948e0f8974a
BALUKUND PANDEY <236516530+balmukundpandey407@users.noreply.github.com>
2026-06-01 16:29:35 +0530

    deployment fixing

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from .routes.tasks import task_router
-from .routes.auth import auth_router
+# from .routes.tasks import task_router
+# from .routes.auth import auth_router
 
 app = FastAPI()
 
@@ -8,5 +8,5 @@ app = FastAPI()
 def home():
     return {"message": "TO DO API IS WORKING PROPERLY"}
 
-app.include_router(task_router)
-app.include_router(auth_router,tags=["AUTHENTICATION_ROUTES"])
+# app.include_router(task_router)
+# app.include_router(auth_router,tags=["AUTHENTICATION_ROUTES"])
