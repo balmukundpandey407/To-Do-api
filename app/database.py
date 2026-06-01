5b3ff437ebab8eb73b94bea0b3c48886d72157cc
BALUKUND PANDEY <236516530+balmukundpandey407@users.noreply.github.com>
2026-06-01 15:54:51 +0530

    deployment fixing

--- app/database.py
+++ app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from decouple import config
+import os
 
-DATABASE_URL = config("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Create engine
 engine = create_engine(DATABASE_URL)
